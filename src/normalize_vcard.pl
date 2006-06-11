395aed3ffaeb9435d1ecac4572410154e56813de
Patrick Ohly <patrick.ohly@gmx.de>
2006-06-11 19:35:24 +0000

    allow sync.scheduleworld.com to replace TZIDs
    git-svn-id: https://zeitsenke.de/svn/SyncEvolution/trunk@137 15ad00c4-1369-45f4-8270-35d70d36bdcd

--- src/normalize_vcard.pl
+++ src/normalize_vcard.pl
@@ -89,6 +89,8 @@ sub Normalize {
       s/^EMAIL;TYPE=\w*/EMAIL/mg;
       # only preserves ORG "Company", but loses "Department" and "Office"
       s/^ORG:([^;]+)(;.*)/ORG:$1/mg;
+      # replaces certain TZIDs with more up-to-date ones
+      s;TZID(=|:)/(scheduleworld.com|softwarestudio.org)/Olson_\d+_\d+/;TZID$1/foo.com/Olson_20000101_1/;mg;
     }
 
 
