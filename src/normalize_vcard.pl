170d6bc6c9699e68a6f0b5999ea4990f371a78b0
Patrick Ohly <patrick.ohly@gmx.de>
2006-12-05 22:24:27 +0000

    avoid tput error when no valid terminal is set
    git-svn-id: https://zeitsenke.de/svn/SyncEvolution/trunk@292 15ad00c4-1369-45f4-8270-35d70d36bdcd

--- src/normalize_vcard.pl
+++ src/normalize_vcard.pl
@@ -196,7 +196,7 @@ sub Normalize {
 # number of columns available for output:
 # try tput without printing the shells error if not found,
 # default to 80
-my $columns = `which tput >/dev/null && tput cols`;
+my $columns = `which tput >/dev/null && tput cols 2>/dev/null`;
 if ($? || !$columns) {
   $columns = 80;
 }
