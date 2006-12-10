e7cd458d37c23871c70d0dddbe263c955fadfbd7
Patrick Ohly <patrick.ohly@gmx.de>
2006-12-10 17:35:20 +0000

    copyright transfer to Funambol
    git-svn-id: https://zeitsenke.de/svn/SyncEvolution/trunk@297 15ad00c4-1369-45f4-8270-35d70d36bdcd

--- src/TestEvolution.cpp
+++ src/TestEvolution.cpp
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2005 Patrick Ohly
+ * Copyright (C) 2005-2006 Patrick Ohly
+ * Copyright (C) 2007 Funambol
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
