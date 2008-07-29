5e8fa6eb94f89dd400a9314c58745bf093c74e54
Patrick Ohly <patrick.ohly@gmx.de>
2008-07-29 21:06:11 +0000

    major restructuring of the file layout and autotools build
    This change makes it possible to add a new backend without
    changing any of the core files. It also gets rid of some
    hacks (like -export-dynamic for the binary) by putting all
    core SyncEvolution code into a library.
    
    The transition is not quite complete: there are still
    some lists of existing backends, which will be removed
    soon. EvolutionSmartPtr.h and EvolutionSyncSource depend
    on Evolution/GNOME libs, which forces all backends to
    use the right -I flags.
    
    
    git-svn-id: https://zeitsenke.de/svn/SyncEvolution/trunk@684 15ad00c4-1369-45f4-8270-35d70d36bdcd

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+set -e
+
+# generate configure.in from main configure-*.in pieces
+# and all backend configure-sub.in pieces
+rm -f configure.in
+cat configure-pre.in >>configure.in
+BACKENDS=
+for sub in src/backends/*/configure-sub.in; do
+    BACKENDS="$BACKENDS `dirname $sub | sed -e 's;^src/;;'`"
+    echo "# vvvvvvvvvvvvvv $sub vvvvvvvvvvvvvv" >>configure.in
+    cat $sub >>configure.in
+    echo "AC_CONFIG_FILES(`echo $sub | sed -e s/configure-sub.in/Makefile/`)" >>configure.in
+    echo "# ^^^^^^^^^^^^^^ $sub ^^^^^^^^^^^^^^" >>configure.in
+    echo >>configure.in
+done
+cat configure-post.in >>configure.in
+
+sed -e "s;@BACKEND_REGISTRIES@;`echo src/backends/*/*Register.cpp | sed -e s%src/%%g`;" \
+    -e "s;@BACKENDS@;$BACKENDS;" \
+     src/Makefile.am.gen >src/Makefile.am
+
 libtoolize -c
 aclocal
 autoheader
