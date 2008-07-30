63e14ddc9c62893dfe39731039121103510e5e89
Patrick Ohly <patrick.ohly@gmx.de>
2008-07-30 17:30:10 +0000

    file layout restructuring, take II
    Added missing files, finished automatic registration
    of backends, Emacs mode for renamed files.
    
    
    git-svn-id: https://zeitsenke.de/svn/SyncEvolution/trunk@685 15ad00c4-1369-45f4-8270-35d70d36bdcd

--- autogen.sh
+++ autogen.sh
@@ -7,8 +7,10 @@ set -e
 rm -f configure.in
 cat configure-pre.in >>configure.in
 BACKENDS=
+SUBS=
 for sub in src/backends/*/configure-sub.in; do
     BACKENDS="$BACKENDS `dirname $sub | sed -e 's;^src/;;'`"
+    SUBS="$SUBS $sub"
     echo "# vvvvvvvvvvvvvv $sub vvvvvvvvvvvvvv" >>configure.in
     cat $sub >>configure.in
     echo "AC_CONFIG_FILES(`echo $sub | sed -e s/configure-sub.in/Makefile/`)" >>configure.in
@@ -19,7 +21,10 @@ cat configure-post.in >>configure.in
 
 sed -e "s;@BACKEND_REGISTRIES@;`echo src/backends/*/*Register.cpp | sed -e s%src/%%g`;" \
     -e "s;@BACKENDS@;$BACKENDS;" \
-     src/Makefile.am.gen >src/Makefile.am
+     src/Makefile-gen.am >src/Makefile.am
+
+sed -e "s;@CONFIG_SUBS@;$SUBS;" \
+    Makefile-gen.am >Makefile.am
 
 libtoolize -c
 aclocal
