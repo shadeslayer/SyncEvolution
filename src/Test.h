c07102e264486af1789a5a96d6d6e6b5e066135c
Patrick Ohly <patrick.ohly@gmx.de>
2008-07-31 19:57:57 +0000

    moved boost headers to src/core because that has to be in the include path anyway
    git-svn-id: https://zeitsenke.de/svn/SyncEvolution/trunk@689 15ad00c4-1369-45f4-8270-35d70d36bdcd

--- src/Test.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-using namespace std;
-
-// until a better solution is found use the helper function from TestMain.cpp
-extern const string &getCurrentTest();
-
-// removes special characters
-extern void simplifyFilename(string &filename);
