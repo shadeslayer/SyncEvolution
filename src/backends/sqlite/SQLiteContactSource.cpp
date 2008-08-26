5fa63c04a6b2b5435e3821216c845ed7026b6a44
Patrick Ohly <patrick.ohly@gmx.de>
2008-08-26 17:45:28 +0000

    license and copyright clarification
    The source was always meant to be GPL v2 or later, but wasn't marked
    consistently as such. Copyright belongs to Patrick Ohly, with all
    code up to 0.7 also owned by Funambol due to a copyright transfer
    at that time.
    
    
    git-svn-id: https://zeitsenke.de/svn/SyncEvolution/trunk@739 15ad00c4-1369-45f4-8270-35d70d36bdcd

--- src/backends/sqlite/SQLiteContactSource.cpp
+++ src/backends/sqlite/SQLiteContactSource.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2007 Patrick Ohly
- * Copyright (C) 2007 Funambol
+ * Copyright (C) 2007-2008 Patrick Ohly
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
