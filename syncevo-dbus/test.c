9ba82d7584b01399965fe783ef39f34350e4ef40
Jussi Kukkonen <jku@linux.intel.com>
2009-04-01 10:52:49 +0300

    move dbus test app to syncevo-dbus/

--- syncevo-dbus/test.c
+++ syncevo-dbus/test.c
@@ -1,49 +1,92 @@
-/* test program for the C wrapper for the SyncEvolution DBus interface */
+/* test syncevo dbus */
 
-#include "syncevo-dbus.h"
+#include <syncevo-dbus/syncevo-dbus.h>
 
-
-void
-progress_cb (SyncevoService *service, char *server, int type, int extra1, int extra2, int extra3)
+static void
+print_option (SyncevoOption *option, gpointer userdata)
 {
-	g_print ("Got signal 'progress': %s: %d %d %d %d\n",
-	         server, type, extra1, extra2, extra3);
+	const char *ns, *key, *value;
+
+	syncevo_option_get (option, &ns, &key, &value);
+	g_debug ("  Got option [%s] %s = %s", ns, key, value);
 }
 
-int main ()
+static void
+progress_cb (SyncevoService *service,
+             char *server,
+             int type,
+             int extra1, int extra2, int extra3)
 {
-	SyncevoService *service;
-	SyncevoSource *source;
-	GPtrArray *sources;
-	GMainLoop *loop;
-	GError *error = NULL;
-	char* source_name = "calendar"; 
-	int source_mode = 1;
+    g_debug ("  progress...");
+}
 
+static void
+source_progress_cb (SyncevoService *service,
+                    char *server,
+                    char *source,
+                    int type,
+                    int extra1, int extra2, int extra3)
+{
+    g_debug ("  source progress...");
+}
 
-	g_type_init ();
-	
-	service = syncevo_service_get_default();
+int main (int argc, char *argv[])
+{
+    SyncevoService *service;
+    GMainLoop *loop;
+    GPtrArray *sources;
+    GError *error = NULL;
+    GPtrArray *options;
+    char **servers;
+    char **ptr;
+    char *server = NULL;
 
-	/*for some reason the dbus type system fails unless a connection has been created... 
-	  workaround: create SyncEvoService first */
-	source = syncevo_source_new (source_name, source_mode);
-	sources = g_ptr_array_new ();
-	g_ptr_array_add (sources, source);
+    g_type_init();
 
-	g_signal_connect (service, "progress", 
-	                  G_CALLBACK (progress_cb), NULL);
-	if (!syncevo_service_start_sync (service, "scheduleworld", sources, &error)) {
-		g_printerr ("StartSync failed :%s\n", error->message);
-		g_error_free (error);
-	} else {
-		g_print ("StartSync ok, waiting for signals\n");
-		loop = g_main_loop_new (NULL, FALSE);
-		g_main_loop_run (loop);
-		g_main_loop_unref (loop);
-	}
+    if (argc > 1) {
+        server = argv[1];
+    }
 
-	g_ptr_array_free (sources, FALSE);
-	syncevo_source_free (source);
-	g_object_unref (service);
+    service = syncevo_service_get_default ();
+
+    g_debug ("Testing syncevo_service_get_servers() ");
+    syncevo_service_get_servers (service, &servers, &error);
+    if (error) {
+        g_error ("  syncevo_service_get_servers() failed with %s", error->message);
+    }
+    
+    for (ptr = servers; *ptr; ptr++) {
+        g_debug ("  Got server '%s'", *ptr);
+    }
+
+    if (!server) {
+        g_print ("No server given, stopping here\n");
+        return 0;
+    }
+    
+    options = g_ptr_array_new();
+    g_debug ("Testing syncevo_service_get_config() with server %s", server);
+    syncevo_service_get_server_config (service, server, &options, &error);
+    if (error) {
+        g_error ("  syncevo_service_get_server_config() failed with %s", error->message);
+    }
+    g_ptr_array_foreach (options, (GFunc)print_option, NULL);
+
+    loop = g_main_loop_new (NULL, TRUE);
+    g_signal_connect (service, "progress", (GCallback)progress_cb, NULL);
+    g_signal_connect (service, "source-progress", (GCallback)source_progress_cb, NULL);
+    
+    g_debug ("Testing syncevo_service_start_sync() with server %s", server);
+    sources = g_ptr_array_new (); /*empty*/
+    syncevo_service_start_sync (service, 
+                                server,
+                                sources,
+                                &error);
+    if (error) {
+        g_error ("  syncevo_service_start_sync() failed with %s", error->message);
+    }
+
+    g_main_loop_run (loop);
+
+    return 0;
 }
