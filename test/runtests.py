98df00d5580301dec0f5ac2a8fde7590fbd639d9
Patrick Ohly <patrick.ohly@intel.com>
2009-07-15 17:02:42 +0200

    testing: fixed typo in recent commit
    The local "evolution" test failed because of a typo
    in the Python function call: comma instead of %.

--- test/runtests.py
+++ test/runtests.py
@@ -384,7 +384,7 @@ class SyncEvolutionTest(Action):
             if context.setupcmd:
                 context.runCommand("%s %s %s %s ./syncevolution" % (self.testenv, self.runner, context.setupcmd, self.name))
             basecmd = "%s SYNC_EVOLUTION_EVO_CALENDAR_DELAY=1 CLIENT_TEST_FAILURES=EvolutionCalendarTest::testOpenDefaultMemo CLIENT_TEST_ALARM=1200 CLIENT_TEST_LOG=%s CLIENT_TEST_EVOLUTION_PREFIX=file://%s/databases %s %s env LD_LIBRARY_PATH=build-synthesis/src/.libs ./client-test" % (self.testenv, self.serverlogs, context.workdir, self.runner, self.testPrefix);
-            context.runCommand("%s testclean", context.make)
+            context.runCommand("%s testclean" % context.make)
             if self.tests:
                 context.runCommand("%s %s" % (basecmd, " ".join(self.tests)))
             else:
