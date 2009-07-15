bb13e0effe7c57fe093c1bb6a0ebc93b4e140273
Patrick Ohly <patrick.ohly@intel.com>
2009-07-15 22:17:28 +0200

    autotools: cleaner separation between permanent and temporary m4 macro copies
    autogen.sh still didn't remove all obsolete copies of libtool m4 macros.
    After moving the only m4 macro that we provide as part of our repository
    into m4-repo, it is possible to rebuild m4 during each autogen.sh run.

--- autogen.sh
+++ autogen.sh
@@ -4,14 +4,14 @@ set -e
 
 # wipe out temporary autotools files, necessary
 # when switching between distros
-rm -rf aclocal.m4 m4/lib* autom4te.cache config.guess config.sub config.h.in configure depcomp install-sh ltmain.sh missing 
+rm -rf aclocal.m4 m4 autom4te.cache config.guess config.sub config.h.in configure depcomp install-sh ltmain.sh missing 
 
 sh ./gen-autotools.sh
 
 libtoolize -c
 glib-gettextize --force --copy
 intltoolize --force --copy --automake
-aclocal -I m4
+aclocal -I m4 -I m4-repo
 autoheader
 automake -a -c -Wno-portability
 autoconf
