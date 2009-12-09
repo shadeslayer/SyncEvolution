244bef12b9a47b97b0ac2084f0771690a192b4c8
Patrick Ohly <patrick.ohly@intel.com>
2009-12-09 13:24:42 +0100

    file backend: clarified documenation (MB #8146)
    The comments for "type" didn't make it clear that the directory
    has to be specified as part of the "evolutionsource" property.

--- src/backends/file/FileSyncSourceRegister.cpp
+++ src/backends/file/FileSyncSourceRegister.cpp
@@ -63,17 +63,20 @@ static RegisterSyncSource registerMe("Files in one directory",
                                      createSource,
                                      "Files in one directory = file\n"
                                      "   Stores items in one directory as one file per item.\n"
-                                     "   The directory is selected via [file://]<path>; it\n"
-                                     "   will only be created if the prefix is given, otherwise\n"
+                                     "   The directory is selected via evolutionsource=[file://]<path>.\n"
+                                     "   It will only be created if the prefix is given, otherwise\n"
                                      "   it must exist already. Only items of the same type can\n"
                                      "   be synchronized and this type must be specified explicitly\n"
                                      "   with both mime type and version.\n"
-                                     "   Examples:\n"
+                                     "   Examples for type:\n"
                                      "      file:text/plain:1.0\n"
                                      "      file:text/x-vcard:2.1\n"
                                      "      file:text/vcard:3.0\n"
                                      "      file:text/x-calendar:1.0\n"
-                                     "      file:text/calendar:2.0\n",
+                                     "      file:text/calendar:2.0\n"
+                                     "   Examples for evolutionsource:\n"
+                                     "      /home/joe/datadir - directory must exist\n"
+                                     "      file:///tmp/scratch - directory is created\n",
                                      Values() +
                                      (Aliases("Files in one directory") + "file"));
 
