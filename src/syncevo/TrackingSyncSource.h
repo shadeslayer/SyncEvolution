ec7f5f63655e2cbfd0075295a9d157a7b16b5fd5
Patrick Ohly <patrick.ohly@intel.com>
2010-01-11 17:43:25 +0100

    TrackingSyncSource: be more quite about getPeerMimeType()
    getPeerMimeType() is implemented by the TrackingSyncSource,
    so implementors of the class don't have to bother with it.
    Removed documentation of the function (which was copied
    from the base class) and moved the function to the "private"
    section with the other implemented functions (because users
    of the TrackingSyncSource API don't need to call it).

--- src/syncevo/TrackingSyncSource.h
+++ src/syncevo/TrackingSyncSource.h
@@ -182,12 +182,6 @@ class TrackingSyncSource : public TestingSyncSource,
      */
     virtual const char *getMimeVersion() const = 0;
 
-    /**
-     * Mime type a backend provides by default, this is used to alert the
-     * remote peer in SAN during server alerted sync.
-     */
-    virtual const char *getPeerMimeType() const;
-
     using SyncSource::getName;
 
   private:
@@ -203,6 +197,7 @@ class TrackingSyncSource : public TestingSyncSource,
     virtual void readItemRaw(const std::string &luid, std::string &item);
     virtual void enableServerMode();
     virtual bool serverModeEnabled() const;
+    virtual const char *getPeerMimeType() const;
 
     boost::shared_ptr<ConfigNode> m_trackingNode;
 };
