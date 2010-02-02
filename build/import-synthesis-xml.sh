d60930129610d3ba4c4f24dbc9d6e267cc4b2c72
Patrick Ohly <patrick.ohly@intel.com>
2010-02-02 15:24:35 +0100

    import-synthesis-xml.sh: import shared XML config fragments (MB #7712)
    These XML config fragments constitute the bulk of what can
    be shared between different apps built on the Synthesis engine.
    The rest is very app specific (like datastores, paths, version, etc).

--- build/import-synthesis-xml.sh
+++ build/import-synthesis-xml.sh
@@ -4,7 +4,7 @@
 # syncevolution git repository. Pass the path
 # to a synthesis repository (default: ../libsynthesis).
 #
-# The script switches to the "synthesis" branch
+# The script switches to the "synthesis-xml-fragments" branch
 # in the syncevolution repo and then merges all
 # patches committed to the "master" branch in the
 # synthesis repo, updating the "synthesis" branch
@@ -17,4 +17,7 @@
 set -e
 set -x
 
-`dirname $0`/import-foreign-git.sh "${1:-../libsynthesis}" synthesis src 3 src/sysync_SDK/configs/syncclient_sample_config.xml
+path="${1:-../libsynthesis}"
+files="`cd $path && find src/sysync_SDK/configs/ \( -name '*.xml' -o -name 'update-samples.pl' -o -name README \) -a \! \( -name 'sync*_sample_config.xml' -o -name sunbird_client.xml \)`"
+
+`dirname $0`/import-foreign-git.sh "${1:-../libsynthesis}" synthesis-xml-fragments src/syncevo/configs 3 $files
