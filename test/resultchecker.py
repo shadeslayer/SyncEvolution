3fb68fe84ae8a4f8b3cba1a3c6e192c38cdbede7
Chen Congwu <congwu.chen@intel.com>
2010-04-17 13:56:00 +0800

    resultchecker: wrote 'calendar_todo' as tag name for 'calendar+todo'
    '+' is a special character in the tag name, let's avoid it.

--- test/resultchecker.py
+++ test/resultchecker.py
@@ -244,7 +244,10 @@ def step2(resultdir, result, servers, indents, srcdir, shellprefix, backenddir):
                 indent +=space
                 indents.append(indent)
                 prefix = logprefix[format]
-                result.write(indent+'<'+format+' prefix="'+prefix+'">\n')
+                qformat = format;
+                if (qformat.find('calendar+todo') !=-1):
+                    qformat = format.replace('calendar+todo', 'calendar_todo')
+                result.write(indent+'<'+qformat+' prefix="'+prefix+'">\n')
                 for case in logdic[format]:
                     indent +=space
                     indents.append(indent)
@@ -259,7 +262,7 @@ def step2(resultdir, result, servers, indents, srcdir, shellprefix, backenddir):
                     result.write('</'+casename+'>\n')
                     indents.pop()
                     indent = indents[-1]
-                result.write(indent+'</'+format+'>\n')
+                result.write(indent+'</'+qformat+'>\n')
                 indents.pop()
                 indent = indents[-1]
             result.write(indent+'</'+server+'>\n')
