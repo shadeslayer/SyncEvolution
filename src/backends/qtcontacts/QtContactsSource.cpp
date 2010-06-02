a61d91b9405efa66a023495012c980ea1674e567
Patrick Ohly <patrick.ohly@intel.com>
2010-06-02 08:28:46 +0200

    QtContacts: first complete (but not usable) backend
    Import/export is done via QtVersit.
    
    Compiling it expects QtContacts/QtVersit in the same path as the rest
    of Qt.

--- src/backends/qtcontacts/QtContactsSource.cpp
+++ src/backends/qtcontacts/QtContactsSource.cpp
@@ -24,13 +24,82 @@
 
 #include "QtContactsSource.h"
 
+#include <QApplication>
+
 #include <QContact>
 #include <QContactManager>
+#include <QContactFetchRequest>
+#include <QContactRemoveRequest>
+#include <QContactSaveRequest>
+#include <QContactTimestamp>
+#include <QContactLocalIdFilter>
+
+#include <QVersitContactExporter>
+#include <QVersitContactImporter>
+#include <QVersitDocument>
+#include <QVersitWriter>
+#include <QVersitReader>
+
+#include <syncevo/SmartPtr.h>
 
 #include <syncevo/declarations.h>
 SE_BEGIN_CXX
 
 using namespace QtMobility;
+
+class QtContactsData
+{
+    QString m_managerURI;
+    cxxptr<QContactManager> m_manager;
+
+    // needed when using Qt code
+    static QApplication *m_app;
+
+public:
+    QtContactsData(const QString &managerURI) :
+        m_managerURI(managerURI)
+    {
+        if (!m_app) {
+            static const char *argv[] = { "SyncEvolution" };
+            static int argc = 1;
+            m_app = new QApplication(argc, (char **)argv);
+        }
+    }
+
+    static QList<QContactLocalId> createContactList(const string &uid)
+    {
+        QList<QContactLocalId> list;
+        list.append(atoi(uid.c_str()));
+        return list;
+    }
+
+    static QContactLocalIdFilter createFilter(const string &uid)
+    {
+        QContactLocalIdFilter filter;
+        filter.setIds(createContactList(uid));
+        return filter;
+    }
+
+    static string getLUID(const QContact &contact)
+    {
+        QContactLocalId id = contact.localId();
+        return StringPrintf("%u", id);
+    }
+
+    static string getRev(const QContact &contact)
+    {
+        QContactTimestamp rev = contact.detail(QContactTimestamp::DefinitionName);
+        QDateTime stamp = rev.lastModified();
+        if (!stamp.isValid()) {
+            stamp = rev.created();
+        }
+        return stamp.toString().toLocal8Bit().constData();
+    }
+
+    friend class QtContactsSource;
+};
+
+QApplication *QtContactsData::m_app;
 
 QtContactsSource::QtContactsSource(const SyncSourceParams &params) :
     TrackingSyncSource(params)
@@ -40,11 +109,20 @@ QtContactsSource::QtContactsSource(const SyncSourceParams &params) :
 
 QtContactsSource::~QtContactsSource()
 {
-    // delete m_data;
+    delete m_data;
 }
 
 void QtContactsSource::open()
 {
+    m_data = new QtContactsData("qtcontacts:tracker:");
+    cxxptr<QContactManager> manager(QContactManager::fromUri(m_data->m_managerURI),
+                                    "QTContactManager");
+    if (manager->error()) {
+        throwError(StringPrintf("failed to open QtContact database %s, error code %d",
+                                m_data->m_managerURI.toLocal8Bit().constData(),
+                                manager->error()));
+    }
+    m_data->m_manager = manager;
 }
 
 bool QtContactsSource::isEmpty()
@@ -54,6 +132,7 @@ bool QtContactsSource::isEmpty()
 
 void QtContactsSource::close()
 {
+    m_data->m_manager.set(0);
 }
 
 QtContactsSource::Databases QtContactsSource::getDatabases()
@@ -62,28 +141,93 @@ QtContactsSource::Databases QtContactsSource::getDatabases()
     QStringList availableManagers = QContactManager::availableManagers();
 
     result.push_back(Database("select database via QtContacts Manager URL",
-                              "[file://]<path>"));
+                              "qtcontacts:tracker:"));
     return result;
 }
 
 void QtContactsSource::listAllItems(RevisionMap_t &revisions)
 {
+    QContactFetchRequest fetch;
+    fetch.setManager(m_data->m_manager.get());
+    // TODO: fetch.setDefinitionRestrictions()
+    fetch.start();
+    fetch.waitForFinished();
+    // TODO: fetch.errors()
+    foreach (const QContact &contact, fetch.contacts()) {
+        revisions[QtContactsData::getLUID(contact)] = QtContactsData::getRev(contact);
+    }
 }
 
 void QtContactsSource::readItem(const string &uid, std::string &item, bool raw)
 {
+    QContactFetchRequest fetch;
+    fetch.setManager(m_data->m_manager.get());
+    fetch.setFilter(QtContactsData::createFilter(uid));
+    fetch.start();
+    fetch.waitForFinished();
+    QContact contact = fetch.contacts().first();
+    QVersitContactExporter exporter;
+    if (!exporter.exportContacts(fetch.contacts(), QVersitDocument::VCard30Type)) {
+        throwError(uid + ": encoding as vCard 3.0 failed");
+    }
+    QByteArray vcard;
+    QVersitWriter writer(&vcard);
+    if (!writer.startWriting(exporter.documents())) {
+        throwError(uid + ": writing as vCard 3.0 failed");
+    }
+    writer.waitForFinished();
+    item = vcard.constData();
+    // TODO: writer.errors() ?
 }
 
 TrackingSyncSource::InsertItemResult QtContactsSource::insertItem(const string &uid, const std::string &item, bool raw)
 {
-    return InsertItemResult("",
-                            "",
-                            false /* true if adding item was turned into update */);
+    QVersitReader reader(QByteArray(item.c_str()));
+    if (!reader.startReading()) {
+        throwError("reading vCard failed");
+    }
+    reader.waitForFinished();
+    // TODO: error?
+
+    QVersitContactImporter importer;
+    if (!importer.importDocuments(reader.results())) {
+        throwError("importing vCard failed");
+    }
+
+    QList<QContact> contacts = importer.contacts();
+    QContact &contact = contacts.first();
+
+    if (!uid.empty()) {
+        QContactId id;
+        id.setManagerUri(m_data->m_managerURI);
+        id.setLocalId(atoi(uid.c_str()));
+        contact.setId(id);
+    }
+
+    QContactSaveRequest save;
+    save.setManager(m_data->m_manager.get());
+    save.setContacts(QList<QContact>() << contact);
+    save.start();
+    save.waitForFinished();
+    // TODO: save.errors();
+
+    QList<QContact> savedContacts = save.contacts();
+    QContact &savedContact = savedContacts.first();
+
+    return InsertItemResult(QtContactsData::getLUID(savedContact),
+                            QtContactsData::getRev(savedContact),
+                            false);
 }
 
 
 void QtContactsSource::removeItem(const string &uid)
 {
+    QContactRemoveRequest remove;
+    remove.setManager(m_data->m_manager.get());
+    remove.setContactIds(QtContactsData::createContactList(uid));
+    remove.start();
+    remove.waitForFinished();
+    // TODO: remove.errors()
 }
 
 SE_END_CXX
