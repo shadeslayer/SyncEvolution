d016a91499ba940e2a11395ebf18fba4a4c090dd
Patrick Ohly <patrick.ohly@intel.com>
2010-12-01 12:10:33 +0100

    merge: carddav

---
+++ src/backends/webdav/CardDAVSource.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2010 Intel Corporation
+ */
+
+#ifndef INCL_CARDDAVSOURCE
+#define INCL_CARDDAVSOURCE
+
+#include <config.h>
+
+#ifdef ENABLE_DAV
+
+#include "WebDAVSource.h"
+#include <syncevo/MapSyncSource.h>
+#include <syncevo/eds_abi_wrapper.h>
+#include <syncevo/SmartPtr.h>
+
+#include <boost/utility.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <syncevo/declarations.h>
+SE_BEGIN_CXX
+
+class CardDAVSource : public WebDAVSource,
+    public SyncSourceLogging
+{
+ public:
+    CardDAVSource(const SyncSourceParams &params, const boost::shared_ptr<SyncEvo::Neon::Settings> &settings);
+
+    /* implementation of SyncSourceSerialize interface */
+    virtual const char *getMimeType() const { return "text/vcard"; }
+    virtual const char *getMimeVersion() const { return "3.0"; }
+
+    // implementation of SyncSourceLogging callback
+    virtual std::string getDescription(const string &luid);
+
+ protected:
+    // implementation of WebDAVSource callbacks
+    virtual bool typeMatches(const StringMap &props) const;
+    virtual std::string homeSetProp() const { return "urn:ietf:params:xml:ns:carddav:addressbook-home-set"; }
+    virtual std::string contentType() const { return "text/vcard; charset=utf-8"; }
+    virtual const std::string *createResourceName(const std::string &item, std::string &buffer, std::string &luid);
+    virtual const std::string *setResourceName(const std::string &item, std::string &buffer, const std::string &luid);
+
+ private:
+    std::string extractUID(const std::string &item);
+};
+
+SE_END_CXX
+
+#endif // ENABLE_DAV
+#endif // INCL_CARDDAVSOURCE
