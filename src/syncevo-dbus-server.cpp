dd1713a5cb332151f294b0a191338b9fe17bab9e
Patrick Ohly <patrick.ohly@intel.com>
2010-12-10 16:34:42 +0100

    syncevo-dbus-server: fixed compile error
    syncevo-dbus-server provides a logger which cannot be used after forking.
    Declare that by adding the isProcessSafe() method.

--- src/syncevo-dbus-server.cpp
+++ src/syncevo-dbus-server.cpp
@@ -1394,6 +1394,8 @@ public:
                           const char *function,
                           const char *format,
                           va_list args);
+
+    virtual bool isProcessSafe() const { return false; }
 };
 
 /**
