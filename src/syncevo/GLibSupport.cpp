53cd5598ffe6b98a65f1e9c4a0edc9c7b665ad77
Patrick Ohly <patrick.ohly@intel.com>
2011-02-15 16:34:46 +0100

    glib: emulate select() call while keeping the event loop running
    LocalTransportAgent needs to wait temporarily for message transfers.
    Without other activity, that is done with select(). But inside syncevo-dbus-server,
    the main glib event loop must be served while waiting. GLibSelect()
    does that by setting up a source which returns a) after the timeout
    or b) when the file descriptor is ready for the next read or write.
    
    As a special case, other returns from the event loop must also be reported.

---
+++ src/syncevo/GLibSupport.cpp
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2011 Intel Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+#include <syncevo/GLibSupport.h>
+#include <syncevo/util.h>
+
+#include <string.h>
+
+SE_BEGIN_CXX
+
+#ifdef HAVE_GLIB
+
+class Select {
+    GMainLoop *m_loop;
+    GMainContext *m_context;
+    struct FDSource;
+    FDSource *m_source;
+    time_t m_deadline;
+    GPollFD m_pollfd;
+    GLibSelectResult m_result;
+
+    struct FDSource
+    {
+        GSource m_source;
+        Select *m_select;
+
+        static gboolean prepare(GSource *source,
+                                gint *timeout)
+        {
+            FDSource *fdsource = (FDSource *)source;
+            time_t now = time(NULL);
+            if (now < fdsource->m_select->m_deadline) {
+                *timeout = (fdsource->m_select->m_deadline - now) * 1000;
+                return FALSE;
+            } else {
+                fdsource->m_select->m_result = GLIB_SELECT_TIMEOUT;
+                *timeout = 0;
+                return TRUE;
+            }
+        }
+
+        static gboolean check(GSource *source)
+        {
+            FDSource *fdsource = (FDSource *)source;
+            if (fdsource->m_select->m_pollfd.revents) {
+                fdsource->m_select->m_result = GLIB_SELECT_READY;
+                return TRUE;
+            } else {
+                return FALSE;
+            }
+        }
+
+        static gboolean dispatch(GSource *source,
+                                 GSourceFunc callback,
+                                 gpointer user_data)
+        {
+            FDSource *fdsource = (FDSource *)source;
+            g_main_loop_quit(fdsource->m_select->m_loop);
+            return FALSE;
+        }
+
+        static GSourceFuncs m_funcs;
+    };
+
+public:
+    Select(GMainLoop *loop, int fd, int direction, int seconds) :
+        m_loop(loop),
+        m_context(g_main_loop_get_context(loop)),
+        // TODO: do not depend on non-linear system time for timeouts (also applies to transports)
+        m_deadline(seconds >= 0 ? time(NULL) + seconds : 0),
+        m_result(GLIB_SELECT_QUIT)
+    {
+        memset(&m_pollfd, 0, sizeof(m_pollfd));
+        m_source = (FDSource *)g_source_new(&FDSource::m_funcs, sizeof(FDSource));
+        if (!m_source) {
+            SE_THROW("no FDSource");
+        }
+        m_source->m_select = this;
+        m_pollfd.fd = fd;
+        if (fd >= 0 &&
+            direction != GLIB_SELECT_NONE) {
+            if (direction & GLIB_SELECT_READ) {
+                m_pollfd.events |= G_IO_IN | G_IO_HUP | G_IO_ERR;
+            }
+            if (direction & GLIB_SELECT_WRITE) {
+                m_pollfd.events |= G_IO_OUT | G_IO_ERR;
+            }
+            g_source_add_poll(&m_source->m_source, &m_pollfd);
+        }
+        g_source_attach(&m_source->m_source, m_context);
+    }
+
+    ~Select()
+    {
+        if (m_source) {
+            g_source_destroy(&m_source->m_source);
+        }
+    }
+
+    GLibSelectResult run()
+    {
+        g_main_loop_run(m_loop);
+        return m_result;
+    }
+};
+
+GSourceFuncs Select::FDSource::m_funcs = {
+    Select::FDSource::prepare,
+    Select::FDSource::check,
+    Select::FDSource::dispatch
+};
+
+GLibSelectResult GLibSelect(GMainLoop *loop, int fd, int direction, int seconds)
+{
+    Select instance(loop, fd, direction, seconds);
+    return instance.run();
+}
+
+#else // HAVE_GLIB
+
+GLibSelectResult GLibSelect(GMainLoop *loop, int fd, int direction, int seconds)
+{
+    SE_THROW("GLibSelect() not implemented without glib support");
+    return GLIB_SELECT_QUIT;
+}
+
+#endif // HAVE_GLIB
+
+SE_END_CXX
---
+++ src/syncevo/GLibSupport.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2011 Intel Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+#ifndef INCL_GLIB_SUPPORT
+# define INCL_GLIB_SUPPORT
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#ifdef HAVE_GLIB
+# include <glib-object.h>
+#else
+typedef void *GMainLoop;
+#endif
+
+#include <syncevo/declarations.h>
+SE_BEGIN_CXX
+
+enum {
+    GLIB_SELECT_NONE = 0,
+    GLIB_SELECT_READ = 1,
+    GLIB_SELECT_WRITE = 2
+};
+
+enum GLibSelectResult {
+    GLIB_SELECT_TIMEOUT,      /**< returned because not ready after given amount of time */
+    GLIB_SELECT_READY,        /**< fd is ready */
+    GLIB_SELECT_QUIT          /**< something else caused the loop to quit, return to caller immediately */
+};
+
+/**
+ * Waits for one particular file descriptor to become ready for reading
+ * and/or writing. Keeps the given loop running while waiting.
+ *
+ * @param  loop       loop to keep running; must not be NULL
+ * @param  fd         file descriptor to watch, -1 for none
+ * @param  direction  read, write, both, or none (then fd is ignored)
+ * @param  seconds    timeout in seconds, -1 for no timeout, 0 for immediate return
+ * @return see GLibSelectResult
+ */
+GLibSelectResult GLibSelect(GMainLoop *loop, int fd, int direction, int seconds);
+
+SE_END_CXX
+
+#endif // INCL_GLIB_SUPPORT
+
