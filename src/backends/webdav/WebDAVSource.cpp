08056a35438150e8ad1757ffdad6530ad2a970eb
Patrick Ohly <patrick.ohly@intel.com>
2011-05-10 09:28:30 +0200

    WebDAV: ignore 404 for DELETE requests
    This could be caused by a race condition (item already deleted by some
    other client). In practice it was seen with a confused Google Calendar
    server: event listed by REPORT, but both GET and DELETE fail for
    it. Not much that can be done about that event via CalDAV then, so let's
    ignore it.

--- src/backends/webdav/WebDAVSource.cpp
+++ src/backends/webdav/WebDAVSource.cpp
@@ -1131,6 +1131,10 @@ void WebDAVSource::removeItem(const string &uid)
     case 204:
         // the expected outcome
         break;
+    case 404:
+        // possibly already removed, ignore
+        SE_LOG_DEBUG(this, NULL, "404 - already removed?");
+        break;
     default:
         SE_THROW_EXCEPTION_STATUS(TransportStatusException,
                                   std::string("unexpected status for removal: ") +
