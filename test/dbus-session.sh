8593dbf19c1799d3471387ee44aaa0359cae5c48
Patrick Ohly <patrick.ohly@intel.com>
2011-08-12 18:48:26 +0000

    dbus-session.sh: optionally start EDS under valgrind
    If DBUS_SESSION_SH_EDS_BASE is set to a directory holding
    e-calendar/addressbook-factory and the main command includes
    valgind or valgrindcheck.sh, then the same command is also
    applied to the starting of these two EDS daemons. The total
    result of dbus-session.sh then is the error code from the main
    command (if not 0) or error codes from either of the two daemons.
    
    Killing via SIGINT and SIGTERM is not considered an error, because
    that is how the two daemons have to be terminated by dbus-session.sh.
    SIGKILL is an error because it implies that the daemons did not
    shut down properly or quickly enough.

--- test/dbus-session.sh
+++ test/dbus-session.sh
@@ -17,5 +17,63 @@ trap "kill $DBUS_SESSION_BUS_PID" EXIT
 KEYRING_PID=$!
 trap "kill $KEYRING_PID" EXIT
 
+# If DBUS_SESSION_SH_EDS_BASE is set and our main program runs
+# under valgrind, then also check EDS. DBUS_SESSION_SH_EDS_BASE
+# must be the directory which contains e-addressbook/calendar-factory.
+E_CAL_PID=
+E_BOOK_PID=
+case "$@" in *valgrind*) prefix=`echo $@ | perl -p -e 's;.*?(\S*/?valgrind\S*).*;$1;'`;;
+             *) prefix=;;
+esac
+if [ "$DBUS_SESSION_SH_EDS_BASE" ] && [ "$prefix" ]; then
+    $prefix $DBUS_SESSION_SH_EDS_BASE/e-calendar-factory &
+    E_CAL_PID=$!
+    $prefix $DBUS_SESSION_SH_EDS_BASE/e-addressbook-factory &
+    E_BOOK_PID=$!
+
+    # give daemons some time to start and register with D-Bus
+    sleep 5
+else
+    DBUS_SESSION_SH_EDS_BASE=
+fi
+
 # run program
 "$@"
+res=$?
+echo dbus-session.sh: program returned $res
+
+# Now also shut down EDS, if started by us. Update total result code if any of them
+# failed the valgrind check.
+shutdown () {
+    pid=$1
+    program=$2
+
+    # give process 20 seconds to shut down
+    i=0
+    while [ "$pid" ] && ps | grep -q "$pid" && [ $i -lt 20 ]; do
+	sleep 1
+	i=`expr $i + 1`
+    done
+    if [ "$pid" ] && ps | grep -q "$pid"; then
+	kill -9 "$pid" 2>/dev/null
+    fi
+    wait "$pid"
+    subres=$?
+    case $subres in 0|130|143) true;; # 130 and 143 indicate that it was killed, probably by us
+                    *) echo $program failed with return code $subres
+	               if [ $res -eq 0 ]; then
+                           res=$subres
+                       fi
+		       ;;
+    esac
+}
+
+if [ "$DBUS_SESSION_SH_EDS_BASE" ]; then
+    kill "$E_CAL_PID" 2>/dev/null
+    kill "$E_BOOK_PID" 2>/dev/null
+    shutdown "$E_CAL_PID" "$DBUS_SESSION_SH_EDS_BASE/e-calendar-factory"
+    shutdown "$E_BOOK_PID" "$DBUS_SESSION_SH_EDS_BASE/e-addressbook-factory"
+fi
+
+echo dbus-session.sh: final result $res
+return $res
