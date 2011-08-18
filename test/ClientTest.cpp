2ee2dabca24895444730e82c9cb24b48b6af93bd
Patrick Ohly <patrick.ohly@intel.com>
2011-08-18 10:22:13 +0000

    testing: make Memotoo testLinkedItems* test pass
    Failed because non-recurring events (like the child event) are sent
    with UTC start/end time. That's okay, let's adapt the test data for
    Memotoo.

--- test/ClientTest.cpp
+++ test/ClientTest.cpp
@@ -4728,7 +4728,7 @@ void ClientTest::getTestData(const char *type, Config &config)
                 "END:VEVENT\n"
                 "END:VCALENDAR\n";
 	} else if (server == "memotoo") {
-	    // local time
+	    // local time, except for detached recurrence
 	    config.parentItem =
 	        "BEGIN:VCALENDAR\n"
                 "PRODID:-//Ximian//NONSGML Evolution Calendar//EN\n"
@@ -4755,8 +4755,8 @@ void ClientTest::getTestData(const char *type, Config &config)
                 "BEGIN:VEVENT\n"
                 "UID:20080407T193125Z-19554-727-1-50@gollum\n"
                 "DTSTAMP:20080407T193125Z\n"
-                "DTSTART:20080413T070000\n"
-                "DTEND:20080413T073000\n"
+                "DTSTART:20080413T050000Z\n"
+                "DTEND:20080413T053000Z\n"
                 "TRANSP:OPAQUE\n"
                 "SEQUENCE:XXX\n"
                 "SUMMARY:Recurring: Modified\n"
