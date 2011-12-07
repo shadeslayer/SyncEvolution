2926ca2e623e5b9ac4a30a0902cdb44692fe7255
Patrick Ohly <patrick.ohly@intel.com>
2011-12-07 07:27:16 +0000

    UUID: use rand_r()
    Use rand() instead of the global rand(). This is motivated by two points: first,
    rand() influences global state of the process, which is bad when done by
    a library.
    
    Second, valgrind (only once, though) reported:
    ==1994== Conditional jump or move depends on uninitialised value(s)
    ==1994==    at 0x8C3C7FC: srandom_r (random_r.c:198)
    ==1994==    by 0x8C3C490: srand (random.c:213)
    ==1994==    by 0x6037FE: SyncEvo::UUID::UUID() (util.cpp:322)
    
    Note sure whether switching to rand_r() will change that, it still uses
    the same way of obtaining the seed which was passed to srand().

--- src/syncevo/util.cpp
+++ src/syncevo/util.cpp
@@ -311,6 +311,7 @@ int Execute(const std::string &cmd, ExecuteFlags flags) throw()
 
 UUID::UUID()
 {
+#ifdef USE_SRAND
     static class InitSRand {
     public:
         InitSRand() {
@@ -321,17 +322,32 @@ UUID::UUID()
             }
             srand(seed);
         }
-    } initSRand;
+        int operator () () { return rand(); }
+    } myrand;
+#else
+    static class InitSRand {
+        unsigned int m_seed;
+    public:
+        InitSRand() {
+            ifstream seedsource("/dev/urandom");
+            if (!seedsource.get((char *)&m_seed, sizeof(m_seed))) {
+                m_seed = time(NULL);
+            }
+        }
+        int operator () () { return rand_r(&m_seed); }
+    } myrand;
+#endif
+
 
     char buffer[16 * 4 + 5];
     sprintf(buffer, "%08x-%04x-%04x-%02x%02x-%08x%04x",
-            rand() & 0xFFFFFFFF,
-            rand() & 0xFFFF,
-            (rand() & 0x0FFF) | 0x4000 /* RFC 4122 time_hi_and_version */,
-            (rand() & 0xBF) | 0x80 /* clock_seq_hi_and_reserved */,
-            rand() & 0xFF,
-            rand() & 0xFFFFFFFF,
-            rand() & 0xFFFF
+            myrand() & 0xFFFFFFFF,
+            myrand() & 0xFFFF,
+            (myrand() & 0x0FFF) | 0x4000 /* RFC 4122 time_hi_and_version */,
+            (myrand() & 0xBF) | 0x80 /* clock_seq_hi_and_reserved */,
+            myrand() & 0xFF,
+            myrand() & 0xFFFFFFFF,
+            myrand() & 0xFFFF
             );
     this->assign(buffer);
 }
