d6c87f80d881c3d8a4bc98afeaf0aca9268c1954
Patrick Ohly <patrick.ohly@intel.com>
2011-12-21 14:33:47 +0100

    fork/exec: generic utility class for fork/exec + D-Bus
    This depends on glib and D-Bus support. There is no global switch for
    D-Bus on/off, only one for syncevo-dbus-server on/off, so that is what
    is getting checked for now. The utility class is inside
    libsyncevolution.la, so that library now also depends on
    libgdbussyncevo.la, if enabled.
    
    The actual implementation uses the GDBus client/server utility classes
    abnd g_spawn(). A child watch is used to detect premature exits of the
    child.
    
    The idea was to also have a failure signal. Except for "child quit
    without establishing connection" there is no error which currently has
    to be reported, and that problem is already reported via the onQuit
    signal. Therefore there is no onFailure signal yet.

---
+++ src/syncevo/ForkExec.cpp
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2011 Intel Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+#include "ForkExec.h"
+
+SE_BEGIN_CXX
+
+static const std::string ForkExecEnvVar("SYNCEVOLUTION_FORK_EXEC=");
+
+ForkExec::ForkExec(GMainLoopCXX &loop) :
+    m_loop(loop)
+{
+}
+
+ForkExecParent::ForkExecParent(GMainLoopCXX &loop,
+                               const std::string &helper) :
+    ForkExec(loop),
+    m_helper(helper),
+    m_childPid(0),
+    m_watchChild(NULL)
+{
+}
+
+boost::shared_ptr<ForkExecParent> ForkExecParent::create(GMainLoopCXX &loop,
+                                                         const std::string &helper)
+{
+    boost::shared_ptr<ForkExecParent> forkexec(new ForkExecParent(loop, helper));
+    return forkexec;
+}
+
+ForkExecParent::~ForkExecParent()
+{
+    if (m_watchChild) {
+        // stop watching
+        g_source_destroy(m_watchChild);
+        g_source_unref(m_watchChild);
+    }
+    if (m_childPid) {
+        g_spawn_close_pid(m_childPid);
+    }
+}
+
+void ForkExecParent::start()
+{
+    if (m_watchChild) {
+        SE_THROW("child already started");
+    }
+
+    // boost::shared_ptr<ForkExecParent> me = ...;
+    GDBusCXX::DBusErrorCXX dbusError;
+
+    m_server = GDBusCXX::DBusServerCXX::listen("", &dbusError);
+    if (!m_server) {
+        dbusError.throwFailure("starting server");
+    }
+    m_server->setNewConnectionCallback(boost::bind(&ForkExecParent::newClientConnection, this, _2));
+
+    m_argvStrings.push_back(m_helper);
+    m_argv.reset(AllocStringArray(m_argvStrings));
+    for (char **env = environ;
+         *env;
+         env++) {
+        if (!boost::starts_with(*env, ForkExecEnvVar)) {
+            m_envStrings.push_back(*env);
+        }
+    }
+
+    // pass D-Bus address via env variable
+    m_envStrings.push_back(ForkExecEnvVar + m_server->getAddress());
+    m_env.reset(AllocStringArray(m_envStrings));
+
+    GErrorCXX gerror;
+    if (!g_spawn_async_with_pipes(NULL, // working directory
+                                  static_cast<gchar **>(m_argv.get()),
+                                  static_cast<gchar **>(m_env.get()),
+                                  (GSpawnFlags)((m_helper.find('/') == m_helper.npos ? G_SPAWN_SEARCH_PATH : 0) |
+                                                G_SPAWN_DO_NOT_REAP_CHILD),
+                                  NULL, // child setup function TODO: redirect stdout to stderr where it will be caught by our own output redirection code
+                                  NULL, // child setup user data
+                                  &m_childPid,
+                                  NULL, NULL, NULL, // stdin/out/error pipes
+                                  gerror)) {
+        m_childPid = 0;
+        gerror.throwError("spawning child");
+    }
+
+    // TODO: introduce C++ wrapper around GSource
+    m_watchChild = g_child_watch_source_new(m_childPid);
+    g_source_set_callback(m_watchChild, (GSourceFunc)watchChildCallback, this, NULL);
+    g_source_attach(m_watchChild, g_main_loop_get_context(m_loop.get()));
+}
+
+void ForkExecParent::watchChildCallback(GPid pid,
+                                        gint status,
+                                        gpointer data)
+{
+    ForkExecParent *me = static_cast<ForkExecParent *>(data);
+    me->m_onQuit(status);
+}
+
+void ForkExecParent::newClientConnection(GDBusCXX::DBusConnectionPtr &conn)
+{
+    m_onConnect(conn);
+}
+
+
+void ForkExecParent::stop()
+{
+}
+
+void ForkExecParent::kill()
+{
+}
+
+ForkExecChild::ForkExecChild(GMainLoopCXX &loop) :
+    ForkExec(loop)
+{
+}
+
+boost::shared_ptr<ForkExecChild> ForkExecChild::create(GMainLoopCXX &loop)
+{
+    boost::shared_ptr<ForkExecChild> forkexec(new ForkExecChild(loop));
+    return forkexec;
+}
+
+void ForkExecChild::connect()
+{
+    const char *address = getParentDBusAddress();
+    if (!address) {
+        SE_THROW("cannot connect to parent, was not forked");
+    }
+
+    GDBusCXX::DBusErrorCXX dbusError;
+    GDBusCXX::DBusConnectionPtr conn = dbus_get_bus_connection(address,
+                                                               &dbusError);
+    if (!conn) {
+        dbusError.throwFailure("connecting to server");
+    }
+    m_onConnect(conn);
+}
+
+bool ForkExecChild::wasForked()
+{
+    return getParentDBusAddress() != NULL;
+}
+
+const char *ForkExecChild::getParentDBusAddress()
+{
+    return getenv(ForkExecEnvVar.substr(0, ForkExecEnvVar.size() - 1).c_str());
+}
+
+SE_END_CXX
+
---
+++ src/syncevo/ForkExec.h
@@ -0,0 +1,189 @@
+/*
+ * Copyright (C) 2011 Intel Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+#ifndef INCL_FORK_EXEC
+# define INCL_FORK_EXEC
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#ifdef HAVE_GLIB
+
+#include <syncevo/util.h>
+#include <syncevo/GLibSupport.h>
+#include <syncevo/SmartPtr.h>
+
+#include "gdbus-cxx-bridge.h"
+
+#include <boost/signals2.hpp>
+
+SE_BEGIN_CXX
+
+/**
+ * Utility class which starts a specific helper binary in a second
+ * process. The helper binary is identified via its base name like
+ * "syncevo-dbus-helper", exact location is then determined
+ * automatically, or via an absolute path.
+ *
+ * Direct D-Bus communication is set up automatically. For this to
+ * work, the helper must use ForkExecChild::connect().
+ *
+ * Progress (like "client connected") and failures ("client disconnected")
+ * are reported via boost::signal2 signals. To make progess, the user of
+ * this class must run a glib event loop.
+ *
+ * Note that failures encountered inside the class methods themselves
+ * will be reported via exceptions. Only asynchronous errors encountered
+ * inside the event loop are reported via the failure signal.
+ */
+
+class ForkExec : private boost::noncopyable {
+ public:
+    /**
+     * the glib main loop passed to create() or connect() in one of
+     * the derived classes (owned by the ForkExec instance)
+     */
+    GMainLoop *getLoop() { return m_loop.get(); }
+
+    /**
+     * Called when the D-Bus connection is up and running. It is ready
+     * to register objects that the peer might need. It is
+     * guaranteed that any objects registered now will be ready before
+     * the helper gets a chance to make D-Bus calls.
+     */
+    typedef boost::signals2::signal<void (const GDBusCXX::DBusConnectionPtr &)> OnConnect;
+    OnConnect m_onConnect;
+
+    // TODO: m_onFailure
+
+ protected:
+    ForkExec(GMainLoopCXX &loop);
+
+    GMainLoopCXX m_loop;
+};
+
+/**
+ * The parent side of a fork/exec.
+ */
+class ForkExecParent : public ForkExec
+{
+ public:
+    ~ForkExecParent();
+
+    /**
+     * A ForkExecParent instance must be created via this factory
+     * method and then be tracked in a shared pointer. This method
+     * will not start the helper yet: first connect your slots, then
+     * call start().
+     */
+    static boost::shared_ptr<ForkExecParent> create(GMainLoopCXX &loop,
+                                                    const std::string &helper);
+
+    /**
+     * the helper string passed to create()
+     */
+    std::string getHelper() const { return m_helper; }
+
+    /**
+     * run the helper executable in the parent process
+     */
+    void start();
+
+    /**
+     * request that the child process terminates by sending it a
+     * SIGINT
+     */
+    void stop();
+
+    /**
+     * kill the child process without giving it a chance to shut down
+     * by sending it a SIGKILL
+     */
+    void kill();
+
+    /**
+     * Called when the helper has quit. The parameter of the signal is
+     * the return status of the helper (see waitpid()).
+     */
+    typedef boost::signals2::signal<void (int)> OnQuit;
+    OnQuit m_onQuit;
+
+ private:
+    ForkExecParent(GMainLoopCXX &loop,
+                   const std::string &helper);
+
+    std::string m_helper;
+    boost::shared_ptr<GDBusCXX::DBusServerCXX> m_server;
+    boost::scoped_array<char *> m_argv;
+    std::list<std::string> m_argvStrings;
+    boost::scoped_array<char *> m_env;
+    std::list<std::string> m_envStrings;
+    GPid m_childPid;
+
+    GSource *m_watchChild;
+    static void watchChildCallback(GPid pid,
+                                   gint status,
+                                   gpointer data);
+
+    void newClientConnection(GDBusCXX::DBusConnectionPtr &conn);
+};
+
+/**
+ * The child side of a fork/exec.
+ *
+ * At the moment, the child cannot monitor the parent or kill it.
+ * Might be added (if needed), in which case the corresponding
+ * ForkExecParent members should be moved to the common ForkExec.
+ */
+class ForkExecChild : public ForkExec
+{
+ public:
+    /**
+     * A ForkExecChild instance must be created via this factory
+     * method and then be tracked in a shared pointer. The process
+     * must have been started by ForkExecParent (directly or indirectly)
+     * and any environment variables set by ForkExecParent must still
+     * be set.
+     */
+    static boost::shared_ptr<ForkExecChild> create(GMainLoopCXX &loop);
+
+    /**
+     * initiates connection to parent, connect to ForkExec::m_onConnect
+     * before calling this function to be notified of success and
+     * ForkExec::m_onFailure for failures
+     */
+    void connect();
+
+    /**
+     * true if the current process was created by ForkExecParent
+     */
+    static bool wasForked();
+
+ private:
+    ForkExecChild(GMainLoopCXX &loop);
+
+    static const char *getParentDBusAddress();
+};
+
+SE_END_CXX
+
+#endif // HAVE_GLIB
+#endif // INCL_FORK_EXEC
+
